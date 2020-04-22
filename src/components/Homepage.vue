0ce19cb1e7714b7a77a6a39b712a711ffd351557
artemmarkaryan <artemandriy@gmail.com>
2020-04-22 21:44:24 +0300

    -

---
+++ src/components/Homepage.vue
@@ -0,0 +1,24 @@
+<template>
+    <div id="homepage">
+
+        <navbar></navbar>
+
+        <h1 class="col-3" v-html>— оформляем и&nbsp;продвигаем бизнес в&nbsp;интернете</h1>
+
+
+    </div>
+</template>
+
+<script>
+    import Navbar from "./Navbar";
+    export default {
+        name: "Homepage",
+        components: {
+            Navbar
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,42 @@
+<template>
+    <nav class="container">
+        <div class="row justify-content-between">
+            <img class="navbar__logo" src="../assets/logo.svg" alt="">
+            <div class="d-flex align-items-center">
+                <a v-for="link in links" :key="link.href" :href="link.href"
+                   class="px-3">
+                    {{ link.title }}
+                </a>
+            </div>
+        </div>
+    </nav>
+</template>
+
+<script>
+    export default {
+        name: "Navbar",
+        data: () => {
+            return {
+                links: [
+                    {
+                        title: "Проекты",
+                        href: "#"
+                    },
+                    {
+                        title: "Команда",
+                        href: "#"
+                    }
+                ]
+            }
+        }
+    }
+</script>
+
+<style scoped lang="scss">
+
+    .navbar__logo {
+        height: 170px;
+        padding: 1rem 0;
+    }
+
+</style>
